85fcd9d26f915eb0e7dc38268e593b65e396df55
Chloe Scheffler <chloescheffler@gmail.com>
2022-08-31 16:43:21 -0400

    commit tree b944066442dc75dfde6b48a58aaa2a5c6dbc989b

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(hobby = "roller-skate") {
+    return (`This Saturday, I want to ${hobby}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(event = "go to the office") {
+    return (`This Monday, I will ${event}.`);
+}
+mondayWork();
+mondayWork("work from home");
